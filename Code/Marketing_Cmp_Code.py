3c1c3c4a5db66e5040449c4fb1b60053774d6bba
Brittney Contreras <bcontrer@vols.utk.edu>
2021-01-11 01:11:36 -0500

    FInal pipeline added. Needs train/test set split

--- Code/Marketing_Cmp_Code.py
+++ Code/Marketing_Cmp_Code.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import LabelEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import StratifiedShuffleSplit
@@ -50,8 +51,12 @@ cat_attributes = ["Dt_Customer", "Education", "Marital_Status"]
 One_Hot_attributes = ["Education", "Marital_Status"]
 Dt_Attributes = ['Dt_Customer']
 
-num_cmp_info = campaign_info.drop(cat_attributes, axis=1) 
 cat_cmp_info = campaign_info[One_Hot_attributes]
+
+num_cmp_info = campaign_info.drop(cat_attributes, axis=1)
+
+num_attributes = list(num_cmp_info) 
+
 
 #***************** Categorical Data Pipeline ********************************#
 
@@ -63,7 +68,8 @@ cat_cmp_info = campaign_info[One_Hot_attributes]
 cat_pipeline = Pipeline([
                     ('cat_encoder', ColumnTransformer([
                         ('onehot',OneHotEncoder(),One_Hot_attributes),\
-                        ('Dt', LabelEncoder(), Dt_Attributes),\
+                        ('Dt', LabelEncoder(), Dt_Attributes),\ 
+                        #!!!!!!!!! MAKE A CLASS FOR THIS
                                     ])),\
                     ('customer_remover', remove_customer())
                 ])
@@ -85,4 +91,29 @@ num_pipeline = Pipeline([
 # We put our two pipelines together into a single pipeline that         #
 # handles both our categorical and numerical data.                      #
 
+final_pipeline = ColumnTransformer([
+                ('cat', cat_pipeline(), cat_attributes),\
+                ('num', num_pipeline(), num_attributes),\
+                ])
 
+cmp_info_prepared = final_pipeline.fit_transform(campaign_info)
+#!!!!!!!!!!!!!!!!!! SPLIT DATA FIRST AND THEN APPLY PIPELINE ONLY TO THE TRAINING DATA
+#!!!!!!!!!!!!!!!!!! UNTIL YOU KNOW YOUR ML ALGORITHM WORKS
+
+#********************* Dropping Unnecessary Features *******************#
+
+dropped_attr = ['ID', 'Z_Revenue', 'Z_CostContact']
+cmp_info_final = cmp_info_prepared.drop(dropped_attr, axis=1)
+
+# ???????????? Do I need to drop Dt_Customer? Did the new label replace it ?????????????????
+# ???????????? or did LabelEncoder make a new column? Was it even added to the DataFrame ???
+
+#****************** Splitting Data into Training and Test Set **************#
+
+split = StratifiedShuffleSplit(n_splits = 1, test_size=0.2, random_state=42)
+for train_index, test_index in split.split(cmp_info_final, cmp_info_final["AcceptedAnyCmp"]):
+    strat_train_set = cmp_info_final.loc[train_index]
+    strat_test_set = cmp_info_final.loc[test_index]
+
+
+#**************** 
